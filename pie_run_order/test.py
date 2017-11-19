d8e62e8f110dd808a11d99377d8724fe70c6eaa2
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-11-19 12:15:25 +0000

    add pie run order test

--- pie_run_order/test.py
+++ pie_run_order/test.py
@@ -9,13 +9,12 @@ from pie_run_order.models import PieRunOrder, PieRunOrderEntry
 
 CLIENT_NAME = "test client"
 TEST_PIE_NAME = "test pie"
+TEST_PIE_NAME2 = "test pie"
+
 
 def add_test_pie_run_order():
-    pie = Pie(name=TEST_PIE_NAME, price=1)
-    pie.save()
-
-    pie_run = PieRun(date=timezone.now())
-    pie_run.save()
+    pie = add_pie(TEST_PIE_NAME)
+    pie_run = add_pie_run()
 
     pie_run_order = PieRunOrder(pie_run=pie_run,client_name=CLIENT_NAME)
     pie_run_order.save()
@@ -26,7 +25,50 @@ def add_test_pie_run_order():
     return pie_run_order_entry
 
 
-class Test(TestCase):
+def add_pie_run():
+    pie_run = PieRun(date=timezone.now())
+    pie_run.save()
+    return pie_run
+
+
+def add_pie(pie_name=TEST_PIE_NAME):
+    pie = Pie(name=pie_name, price=1)
+    pie.save()
+    return pie
+
+
+class TestAdd(TestCase):
+    def test_add(self):
+        pie = add_pie()
+        pie_run = add_pie_run()
+        data = {
+            "client_name": CLIENT_NAME,
+            "pie_run_id": pie_run.pie_run_id,
+            "pie-id[]": pie.pie_id,
+            "pie-count[]": 1,
+        }
+        response = self.client.post(reverse("pie_run_order:add"), data=data)
+        # Make sure it redirects, we don't want page reloads to post this again.
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_multiple_pies(self):
+        pie = add_pie(TEST_PIE_NAME)
+        pie2 = add_pie(TEST_PIE_NAME2)
+        pie_run = add_pie_run()
+        data = {
+            "client_name": CLIENT_NAME,
+            "pie_run_id": pie_run.pie_run_id,
+            "pie-id[]": [pie.pie_id, pie2.pie_id],
+            "pie-count[]": [1, 2],
+        }
+        response = self.client.post(reverse("pie_run_order:add"), data=data)
+        # Make sure it redirects, we don't want page reloads to post this again.
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(PieRunOrder.objects.count(), 1)
+        self.assertEqual(PieRunOrderEntry.objects.count(), 2)
+
+
+class TestDetails(TestCase):
     def test_details(self):
         pie_run_order_entry = add_test_pie_run_order()
         response = self.client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
