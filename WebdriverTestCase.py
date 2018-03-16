4269e072d5b31fc825507528f4bbd6c23a990f02
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2018-03-16 23:39:31 +0000

    Try some pytest

--- WebdriverTestCase.py
+++ WebdriverTestCase.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
 
+from django.core.management.commands import runserver
 from selenium import webdriver
 
 
 class WebdriverTestCase(TestCase):
     def setUp(self):
+        runserver.run('127.0.0.1', 8000, runserver.get_internal_wsgi_application())
         self.url = 'http://127.0.0.1:8000/'
         self.browser = webdriver.Firefox()
 
--- pie_run_order/test.py
+++ pie_run_order/test.py
@@ -1,6 +1,6 @@
 import uuid
 
-from django.test import TestCase
+import pytest
 from django.urls import reverse
 from django.utils import timezone
 
@@ -38,53 +38,61 @@ def add_pie(pie_name=TEST_PIE_NAME):
     return pie
 
 
-class TestAdd(TestCase):
-    def test_add(self):
-        pie = add_pie()
-        pie_run = add_pie_run()
-        data = {
-            "client_name": CLIENT_NAME,
-            "pie_run_id": pie_run.pie_run_id,
-            "pie-id[]": pie.pie_id,
-            "pie-count[]": 1,
-        }
-        response = self.client.post(reverse("pie_run_order:add"), data=data)
-        # Make sure it redirects, we don't want page reloads to post this again.
-        self.assertEqual(response.status_code, 302)
-
-    def test_add_multiple_pies(self):
-        pie = add_pie(TEST_PIE_NAME)
-        pie2 = add_pie(TEST_PIE_NAME2)
-        pie_run = add_pie_run()
-        data = {
-            "client_name": CLIENT_NAME,
-            "pie_run_id": pie_run.pie_run_id,
-            "pie-id[]": [pie.pie_id, pie2.pie_id],
-            "pie-count[]": [1, 2],
-        }
-        response = self.client.post(reverse("pie_run_order:add"), data=data)
-        # Make sure it redirects, we don't want page reloads to post this again.
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(PieRunOrder.objects.count(), 1)
-        self.assertEqual(PieRunOrderEntry.objects.count(), 2)
+@pytest.mark.django_db
+def test_add(client):
+    pie = add_pie()
+    pie_run = add_pie_run()
+    data = {
+        "client_name": CLIENT_NAME,
+        "pie_run_id": pie_run.pie_run_id,
+        "pie-id[]": pie.pie_id,
+        "pie-count[]": 1,
+    }
+    response = client.post(reverse("pie_run_order:add"), data=data)
+    # Make sure it redirects, we don't want page reloads to post this again.
+    assert response.status_code == 302
 
 
-class TestDetails(TestCase):
-    def test_details(self):
-        pie_run_order_entry = add_test_pie_run_order()
-        response = self.client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
-        self.assertIs(response.status_code, 200)
+@pytest.mark.django_db
+def test_add_multiple_pies(client):
+    pie = add_pie(TEST_PIE_NAME)
+    pie2 = add_pie(TEST_PIE_NAME2)
+    pie_run = add_pie_run()
+    data = {
+        "client_name": CLIENT_NAME,
+        "pie_run_id": pie_run.pie_run_id,
+        "pie-id[]": [pie.pie_id, pie2.pie_id],
+        "pie-count[]": [1, 2],
+    }
+    response = client.post(reverse("pie_run_order:add"), data=data)
+    # Make sure it redirects, we don't want page reloads to post this again.
+    assert response.status_code == 302
+    assert PieRunOrder.objects.count() == 1
+    assert PieRunOrderEntry.objects.count() == 2
 
-    def test_details_contains_client_name(self):
-        pie_run_order_entry = add_test_pie_run_order()
-        response = self.client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
-        self.assertContains(response, text=TEST_PIE_NAME, count=1)
 
-    def test_details_contains_entries(self):
-        pie_run_order_entry = add_test_pie_run_order()
-        response = self.client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
-        self.assertContains(response, text=CLIENT_NAME, count=1)
+@pytest.mark.django_db
+def test_details(client):
+    pie_run_order_entry = add_test_pie_run_order()
+    response = client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
+    assert response.status_code == 200
 
-    def test_details_returns_404_if_not_found(self):
-        response = self.client.get(reverse("pie_run_order:details", args=(uuid.uuid4(),)))
-        self.assertEqual(response.status_code, 404)
+
+@pytest.mark.django_db
+def test_details_contains_client_name(client):
+    pie_run_order_entry = add_test_pie_run_order()
+    response = client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
+    assert TEST_PIE_NAME in response.rendered_content
+
+
+@pytest.mark.django_db
+def test_details_contains_entries(client):
+    pie_run_order_entry = add_test_pie_run_order()
+    response = client.get(reverse("pie_run_order:details", args=(pie_run_order_entry.pie_run_order_id,)))
+    assert CLIENT_NAME in response.rendered_content
+
+
+@pytest.mark.django_db
+def test_details_returns_404_if_not_found(client):
+    response = client.get(reverse("pie_run_order:details", args=(uuid.uuid4(),)))
+    assert response.status_code == 404
