4fd6a6920b9a33aeb7d5b97f1313f673e275744b
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2018-03-17 21:28:27 +0000

    Pytest all the things

--- pie_run_order/functional_tests.py
+++ pie_run_order/functional_tests.py
@@ -1,8 +1,27 @@
-from WebdriverTestCase import WebdriverTestCase
+import pytest
 
 
-class FunctionalTestDetails(WebdriverTestCase):
-    def test_can_load_page(self):
-        self.browser.get(self.url)
-        body = self.browser.find_element_by_css_selector('body')
-        self.assertNotEqual(body, None)
+@pytest.fixture
+def domain() -> str:
+    return '127.0.0.1'
+
+
+@pytest.fixture
+def port() -> int:
+    return 8000
+
+
+@pytest.fixture
+def url(domain: str, port: int) -> str:
+    return f'http://{domain}:{port}/'
+
+
+# def setup_module(module):
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyPie.settings")
+# execute_from_command_line(['/Users/stefano/dev/PyPie/manage.py', 'runserver', '8000'])
+
+
+def test_can_load_page(selenium, url: str):
+    selenium.get(url)
+    body = selenium.find_element_by_css_selector('body')
+    assert body is not None
