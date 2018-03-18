f5a6d0a0c4d4bd4edc5f6e0f5759bf11bddc4cd2
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2018-03-18 17:58:23 +0000

    Move tests to the correct app

--- pie_run/functional_tests.py
+++ pie_run/functional_tests.py
@@ -17,11 +17,19 @@ def url(domain: str, port: int) -> str:
 
 
 # def setup_module(module):
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyPie.settings")
-# execute_from_command_line(['/Users/stefano/dev/PyPie/manage.py', 'runserver', '8000'])
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyPie.settings")
+#     execute_from_command_line(['/Users/stefano/dev/PyPie/manage.py', 'runserver', '8000'])
 
 
+@pytest.mark.nondestructive
 def test_can_load_page(selenium, url: str):
     selenium.get(url)
     body = selenium.find_element_by_css_selector('body')
     assert body is not None
+
+
+@pytest.mark.nondestructive
+def test_homepage_has_h1_pie_runs(selenium, url: str):
+    selenium.get(url)
+    heading = selenium.find_element_by_css_selector('h1')
+    assert heading.text == 'Pie Runs'
